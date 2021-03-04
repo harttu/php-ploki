efc05b29427ad2258ac7d1ee080ce4d0b3a37c71
harttu <harttu.toivonen@gmail.com>
2021-03-04 02:46:08 -0800

    Create login.php

---
+++ versio3/login.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h3>Tervetuloa matalaan majaan</h3>
+<form action="index.php" method="post">
+Käyttäjätunnus: 
+<input type="text" name="ktunnus"><br>
+Salasana: 
+<input type="password" name="ssana"><br>
+<input type="submit">
+</form>
+</body>
+</html>
