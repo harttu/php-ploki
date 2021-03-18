cb44e7f723307fbeaeb6f2bb1ecf20ec9c940897
harttu <harttu.toivonen@gmail.com>
2021-03-18 10:07:33 +0200

    Update dashboard.php

--- 18_3/dashboard.php
+++ 18_3/dashboard.php
@@ -6,7 +6,8 @@ session_start();
 <?php include 'php_connect_to_mysql.php';?>
 
 <?php
-echo "Tervetuloa, ".$_SESSION["kayttajatunnus"];
+echo "Tervetuloa, ".$_SESSION["kayttajatunnus"]."<br>";
 // Kopio tähän admin osiosta user-tietojen näyttö sessionissa olevalla käyttäjälle.
-
+echo "Session:<br>";
+print_r($_SESSION);
 ?> 
